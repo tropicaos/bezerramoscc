78d6641204a9baea8c6c1d76f432c8d3d4b9c632
Laureano. <laugreenfer@gmail.com>
2025-01-21 07:23:36 -0300

    preloader delete

--- js/glitche-scripts.js
+++ js/glitche-scripts.js
@@ -16,40 +16,6 @@ $(function () {
 	var height = $(window).height();
 	$('.section.started').css({'height': height-60});
 	
-	/* Typed preload text */
-	$('.typed-load').typed({
-		stringsElement: $('.typing-load'),
-		loop: true
-	});
-	
-	/* Preloader 
-	$(window).load(function() {
-		$(".preloader .pre-inner").fadeOut(800, function(){
-			/* Preload hide 
-			$('.preloader').fadeOut();
-			$('body').addClass('loaded');
-			
-			/* Typed subtitle 
-			$('.typed-subtitle').typed({
-				stringsElement: $('.typing-subtitle'),
-				loop: true
-			});
-			
-			/* Typed breadcrumbs 
-			$('.typed-bread').typed({
-				stringsElement: $('.typing-bread'),
-				showCursor: false
-			});
-
-			/* One Page Nav 
-			var url_hash = location.hash;
-			var sectionElem = $(url_hash);
-			if(url_hash.indexOf('#section-') == 0 && sectionElem.length){
-				$('body, html').animate({scrollTop: $(url_hash).offset().top - 70}, 400);
-			}
-		});
-	});
-	
 	/*Fade-out animation between load pages*/
 	$('header .top-menu, .typed-bread').on('click', 'a', function(){
 		var link = $(this).attr('href');
